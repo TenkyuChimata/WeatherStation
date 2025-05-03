578f203aa302462a1df7efb8ddf559a88bd1129e
TenkyuChimata <tenkyuchimata@gmail.com>
2025-05-03 23:04:03 +0800

    Use serial to send data

--- src/main.cpp
+++ src/main.cpp
@@ -1,109 +1,75 @@
 #include <SPI.h>
+#include <Wire.h>
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
 #include <Adafruit_BME280.h>
-#include <ESP8266WebServer.h>
 
-#define SYNC_WORD 0x8A
+#define SYNC_WORD 0x8A // 同步字节，用来标记数据开始
 
-const char *ssid = "";
-const char *password = "";
-
+// 传感器数据结构：3个浮点数 + 1个校验和
 typedef struct {
-  float data[4];
+  float temperature;
+  float humidity;
+  float pressure;
   uint8_t checksum;
 } sensor_st;
 
-sensor_st data;
-Adafruit_BME280 bme;
-ESP8266WebServer server(80);
-unsigned long lastMeasure = 0;
+Adafruit_BME280 bme;           // BME280 传感器对象
+unsigned long lastMeasure = 0; // 上次测量时间戳
 
-uint8_t checksum(float *data, uint8_t length) {
-  uint8_t checksum = 0;
+// 计算浮点数组的异或校验和
+uint8_t calculateChecksum(float *data, uint8_t length) {
+  uint8_t cs = 0;
   for (uint8_t i = 0; i < length; i++) {
     uint8_t *bytes = (uint8_t *)&data[i];
     for (uint8_t j = 0; j < sizeof(float); j++) {
-      checksum ^= bytes[j];
+      cs ^= bytes[j];
     }
   }
-  return checksum;
-}
-
-void checkWiFi() {
-  if (WiFi.status() != WL_CONNECTED) {
-    Serial.println("WiFi connection lost, trying to reconnect...");
-    WiFi.disconnect();
-    WiFi.begin(ssid, password);
-    // 等待连接恢复
-    while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("WiFi reconnected successfully!");
-    IPAddress ip = WiFi.localIP();
-    Serial.print("New IP address: ");
-    Serial.println(ip);
-    server.begin();
-  }
+  return cs;
 }
 
 void setup() {
-  Serial.begin(19200);
-  Wire.begin(4, 5);
+  Serial.begin(19200); // 初始化串口，波特率同原来设定
+  Wire.begin(4, 5);    // I2C 接口，SDA=4, SCL=5
+
+  // 初始化 BME280
   while (!bme.begin(0x76, &Wire)) {
     Serial.println("Could not find a valid BME280 sensor.");
     delay(500);
   }
-
   bme.setSampling(Adafruit_BME280::MODE_FORCED,
                   Adafruit_BME280::SAMPLING_X1,
                   Adafruit_BME280::SAMPLING_X1,
                   Adafruit_BME280::SAMPLING_X1,
                   Adafruit_BME280::FILTER_OFF);
-
-  WiFi.setAutoReconnect(true);
-  WiFi.persistent(true);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.println("Connecting to WiFi...");
-  }
-  Serial.println("Connected to WiFi");
-
-  server.on("/", HTTP_GET, []() {
-    char buffer[256];
-    snprintf(buffer, sizeof(buffer),
-             "{\"temperature\": %f, \"humidity\": %f, \"pressure\": %f}",
-             data.data[0], data.data[1], data.data[2]);
-    server.send(200, "application/json", buffer); });
-
-  IPAddress ip = WiFi.localIP();
-  Serial.print("ESP8266 Web Server's IP address: ");
-  Serial.println(ip);
-  if (ip[0] == 169 && ip[1] == 254) {
-    ESP.reset();
-  }
-  server.begin();
 }
 
 void loop() {
-  server.handleClient();
-
-  checkWiFi();
-
   unsigned long now = millis();
-  if (now - lastMeasure >= 60000) {
+  if (now - lastMeasure >= 60000) { // 每 60 秒测量一次
     lastMeasure = now;
-    sensor_st dat;
+
+    // 强制测量
     bme.takeForcedMeasurement();
 
-    dat.data[0] = bme.readTemperature();
-    dat.data[1] = bme.readHumidity();
-    dat.data[2] = bme.readPressure() / 100.0F;
-    dat.checksum = checksum(dat.data, 3);
+    // 临时存放浮点数据用于校验
+    float buf[3];
+    buf[0] = bme.readTemperature();
+    buf[1] = bme.readHumidity();
+    buf[2] = bme.readPressure() / 100.0F;
 
-    memcpy(&data, &dat, sizeof(sensor_st));
+    // 计算校验和
+    uint8_t cs = calculateChecksum(buf, 3);
+
+    // 准备发送的数据包
+    sensor_st packet;
+    packet.temperature = buf[0];
+    packet.humidity = buf[1];
+    packet.pressure = buf[2];
+    packet.checksum = cs;
+
+    // 通过串口发送：先同步字节，再发送整个结构体的二进制内容
+    Serial.write(SYNC_WORD);
+    Serial.write((uint8_t *)&packet, sizeof(packet));
   }
 }
